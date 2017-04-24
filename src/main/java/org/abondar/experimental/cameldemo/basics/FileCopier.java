0c988f5a65707c8c1d0984e806214ca25b80c77b
abondar24 <abondar1992@gmail.com>
2017-04-24 11:03:17 -0700

    FileCopier

---
+++ src/main/java/org/abondar/experimental/cameldemo/basics/FileCopier.java
@@ -0,0 +1,27 @@
+package org.abondar.experimental.cameldemo.basics;
+
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class FileCopier {
+    public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+
+        RouteBuilder builder = new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:home/abondar/Documents/ss1.doc?noop=true")
+                        .to("file:home/abondar/Downloads/ss1");
+            }
+        };
+
+        context.addRoutes(builder);
+
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+        }
+    }
+
