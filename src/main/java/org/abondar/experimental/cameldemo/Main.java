2bd87bd8367fe9bf3e717a27569470e08347103e
abondar24 <abondar1992@gmail.com>
2020-11-22 22:58:09 +0100

    main fix

--- src/main/java/org/abondar/experimental/cameldemo/Main.java
+++ src/main/java/org/abondar/experimental/cameldemo/Main.java
@@ -7,6 +7,11 @@ public class Main {
     public static void main(String[] args) {
         CommandSwitcher cs = new CommandSwitcher();
 
+        if (args.length==0){
+            System.out.println("Missing argument. Please check documentation for available argynebts");
+            System.exit(0);
+        }
+
         String cmd = args[0].toUpperCase();
 
         cs.executeCommand(cmd);
