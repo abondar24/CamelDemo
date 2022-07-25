39dc9d936b5a0193cb8a27bc131a77c183db4923
abondar24 <abondar1992@gmail.com>
2022-07-26 00:59:32 +0200

    format product api test

--- ShoppingCart/src/test/java/org/abondar/experimental/cameldemo/route/ProductRestTest.java
+++ ShoppingCart/src/test/java/org/abondar/experimental/cameldemo/route/ProductRestTest.java
@@ -2,7 +2,6 @@ package org.abondar.experimental.cameldemo.route;
 
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.client.WebClient;
-import org.abondar.experimental.cameldemo.shoppingcart.model.CartItems;
 import org.abondar.experimental.cameldemo.shoppingcart.model.CartProduct;
 import org.abondar.experimental.cameldemo.shoppingcart.model.CartProductPostResponse;
 import org.abondar.experimental.cameldemo.shoppingcart.processor.ProductProcessor;
@@ -10,7 +9,6 @@ import org.abondar.experimental.cameldemo.shoppingcart.route.FirebaseRoute;
 import org.abondar.experimental.cameldemo.shoppingcart.route.ProductRestRoute;
 import org.abondar.experimental.cameldemo.shoppingcart.transform.ResponseBodyTransformer;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -20,86 +18,79 @@ import org.springframework.test.context.ActiveProfiles;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EnableAutoConfiguration
 @SpringBootTest(
-        properties = {
-                "firebase.products=testProducts.json"
-        },
-        classes = {
-                ProductRestRoute.class,
-                FirebaseRoute.class,
-                ProductProcessor.class,
-                ResponseBodyTransformer.class
-        })
+    properties = {"firebase.products=testProducts.json"},
+    classes = {
+      ProductRestRoute.class,
+      FirebaseRoute.class,
+      ProductProcessor.class,
+      ResponseBodyTransformer.class
+    })
 @ActiveProfiles("integration")
 public class ProductRestTest {
 
-        private static WebClient client;
-        private String testId;
+  private static WebClient client;
+  private String testId;
 
-        @BeforeAll
-        public static void initClient(){
-            Vertx vertx = Vertx.vertx();
-            client = WebClient.create(vertx);
-        }
+  @BeforeAll
+  public static void initClient() {
+    Vertx vertx = Vertx.vertx();
+    client = WebClient.create(vertx);
+  }
 
-        @Test
-        @Order(1)
-        public void postProductTest() {
-                var req = client.post("localhost:8080/cart/product");
-                var testProduct= new CartProduct("test","test", BigDecimal.valueOf(0));
+  @Test
+  @Order(1)
+  public void postProductTest() {
+    var req = client.post("localhost:8080/cart/product");
+    var testProduct = new CartProduct("test", "test", BigDecimal.valueOf(0));
 
-                req.sendJson(testProduct)
-                        .onSuccess(
-                                response -> {
-                                        var body = response.bodyAsJson(CartProductPostResponse.class);
+    req.sendJson(testProduct)
+        .onSuccess(
+            response -> {
+              var body = response.bodyAsJson(CartProductPostResponse.class);
 
-                                        testId=body.name();
-                                        assertEquals(200, response.statusCode());
-                                        assertEquals("test",body.toString());
-                                })
-                        .onFailure(err->{
-                            assertNotNull(err);
-                        });
-        }
+              testId = body.name();
+              assertEquals(200, response.statusCode());
+              assertEquals("test", body.toString());
+            })
+        .onFailure(System.out::println);
+  }
 
-    @Test
-    @Order(2)
-    public void getByIdProductTest() {
-        var req = client.get("localhost:8080/cart/product"+testId);
+  @Test
+  @Order(2)
+  public void getByIdProductTest() {
+    var req = client.get("localhost:8080/cart/product" + testId);
 
-        req.send()
-                .onSuccess(
-                        response -> {
-                            var body = response.bodyAsJson(CartProduct.class);
+    req.send()
+        .onSuccess(
+            response -> {
+              var body = response.bodyAsJson(CartProduct.class);
 
-                            assertEquals(200, response.statusCode());
-                            assertEquals("test",body.getName());
-                            assertEquals("test",body.getImgUrl());
-                            assertEquals(BigDecimal.valueOf(0),body.getPrice());
+              assertEquals(200, response.statusCode());
+              assertEquals("test", body.getName());
+              assertEquals("test", body.getImgUrl());
+              assertEquals(BigDecimal.valueOf(0), body.getPrice());
+            })
+        .onFailure(System.out::println);
+  }
 
-                        })
-                .onFailure(System.out::println);
-    }
+  @Test
+  @Order(2)
+  public void getByLimitProductTest() {
+    var req = client.get("localhost:8080/cart/product");
+    req.setQueryParam("limit", "1");
 
-    @Test
-    @Order(2)
-    public void getByLimitProductTest() {
-        var req = client.get("localhost:8080/cart/product");
-        req.setQueryParam("limit","1");
-
-        req.send()
-                .onSuccess(
-                        response -> {
-                            var body = response.bodyAsJson(CartProduct.class);
-                            assertEquals(200, response.statusCode());
-                            assertEquals("test",body.getName());
-                            assertEquals("test",body.getImgUrl());
-                            assertEquals(BigDecimal.valueOf(0),body.getPrice());
-                        })
-                .onFailure(System.out::println);
-    }
+    req.send()
+        .onSuccess(
+            response -> {
+              var body = response.bodyAsJson(CartProduct.class);
+              assertEquals(200, response.statusCode());
+              assertEquals("test", body.getName());
+              assertEquals("test", body.getImgUrl());
+              assertEquals(BigDecimal.valueOf(0), body.getPrice());
+            })
+        .onFailure(System.out::println);
+  }
 }
