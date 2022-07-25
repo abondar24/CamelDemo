4f9ac67166cf66ec62f46868632f2b9205248846
abondar24 <abondar1992@gmail.com>
2022-07-25 23:31:58 +0200

    extend cart item model

--- ShoppingCart/src/main/java/org/abondar/experimental/cameldemo/shoppingcart/model/CartItems.java
+++ ShoppingCart/src/main/java/org/abondar/experimental/cameldemo/shoppingcart/model/CartItems.java
@@ -1,8 +1,11 @@
 package org.abondar.experimental.cameldemo.shoppingcart.model;
 
+import java.util.List;
+
 public record CartItems(
         boolean changed,
         long itemsTotal,
-        boolean showCart
+        boolean showCart,
+        List<CartItem> items
 ) {
 }
